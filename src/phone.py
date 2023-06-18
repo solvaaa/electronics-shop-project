5334923eea33919a40f365497746699db91a018f
Alexandra <alexandrasolovjova@mail.ru>
2023-06-18 21:01:47 +0300

    change sim_count to number_of_sim

--- src/phone.py
+++ src/phone.py
@@ -1,9 +1,9 @@
 from src.item import Item
 class Phone(Item):
-    def __init__(self, name: str, price: float, quantity: int, sim_count: int):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.sim_count = sim_count
+        self.number_of_sim = number_of_sim
 
     def __repr__(self):
-        return super().__repr__()[:-1] + f', {self.sim_count})'
+        return super().__repr__()[:-1] + f', {self.number_of_sim})'
 
