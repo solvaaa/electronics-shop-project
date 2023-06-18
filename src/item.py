ac07975682505f1c9a772c2f70496f570f1b0ba4
Alexandra <alexandrasolovjova@mail.ru>
2023-06-18 21:26:36 +0300

    added Exception to Item.name

--- src/item.py
+++ src/item.py
@@ -46,7 +46,7 @@ class Item:
         if len(name) <= 10:
             self.__name = name
         else:
-            print("Exception: Длина наименования товара превышает 10 символов")
+            raise Exception("Длина наименования товара превышает 10 символов")
 
     @classmethod
     def instantiate_from_csv(cls):
