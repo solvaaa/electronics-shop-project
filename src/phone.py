d3d965e9f137e81b5f64c53a134fdeef454a0d7e
Alexandra <alexandrasolovjova@mail.ru>
2023-06-18 21:24:36 +0300

    added number_of_sim property

--- src/phone.py
+++ src/phone.py
@@ -2,8 +2,19 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
     def __repr__(self):
         return super().__repr__()[:-1] + f', {self.number_of_sim})'
 
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim):
+        if number_of_sim > 0 and isinstance(number_of_sim, int):
+            self.__number_of_sim = number_of_sim
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+            #print("Exception: Длина наименования товара превышает 10 символов")
